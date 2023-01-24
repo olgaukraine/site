79e9418bdfa9a1ec922d37c4e6a013aada8d7cb4
olgaukraine <123236022+olgaukraine@users.noreply.github.com>
2023-01-24 18:10:03 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+/*alert("Hello world!");
+alert("Привіт світ!");
+alert(3+
+	25+
+	11) //тут у мене сума чисел
+let text;
+
+text="Hello world!";
+alert(text)
+let text = 'Привіт, Світ!';   
+let message;   
+message = text; // скопіювати значення  
+alert(text); // Привіт, Світ!   
+alert(message); // Привіт, Світ! 
+let імя = "Петро"; 
+alert( імя ); // "Петро"
+let return1 = 5; // помилка
+alert(return1); 
+let ourPlanetName="Земля", userName="Василь"
+alert(ourPlanetName); alert(userName)
+let str1= "В JavaScript є тільки тип «рядок» string"; 
+let str2 = 'В JavaScript тип символ не існує';
+alert(str1)
+alert(str2)
+let str1="де ", str2="життя ", str3="є "
+document.write("<p>"+str1+str3+str2+"</p>")*/
+let name=prompt("What is your name?","Olga")
+document.write("<p>"+"Hello dear "+name+"!"+"</p>")
